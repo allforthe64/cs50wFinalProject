cf3c8106c736d966cb66c096ca045e0c8b81dc4a
allforthe64 <williamhrainey@gmail.com>
2021-10-12 12:23:13 -0700

    got title headers working

--- finalProject/TSW2/urls.py
+++ finalProject/TSW2/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("gameplayVSrealLifeComparison", views.comparison, name="comparison")
 ]
--- finalProject/TSW2/views.py
+++ finalProject/TSW2/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "TSW2/index.html")
+
+def comparison(request):
+    return render(request, "TSW2/comparison.html")
