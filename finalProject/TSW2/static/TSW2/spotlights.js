797bf442dcc1c0c13e100e8837791e3d30d2e708
allforthe64 <williamhrainey@gmail.com>
2021-10-14 14:08:21 -0700

    added article switching mechanic/working on finishing styling

--- finalProject/TSW2/static/TSW2/spotlights.js
+++ finalProject/TSW2/static/TSW2/spotlights.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Js is working!");
+    
+    //query page for all buttons
+    document.querySelectorAll("#selectorButton").forEach(item => {
+        item.addEventListener("click", event => {
+            
+            //set target equal to the location that should be shown
+            var target = item.value; 
+
+            //query for article divs
+            document.querySelectorAll(".article").forEach(article => {
+                
+                //check to see if target is equal to article's id
+                if (article.id == target) {
+                    //display article
+                    article.style.display = 'block';
+                }
+                else {
+                    //hide article
+                    article.style.display = 'none';
+                }
+            })
+        })
+    })
 })
